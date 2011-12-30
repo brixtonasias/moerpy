0e2aaf1db46f95681c376bfb47c7d8880a2c9fc1
Stefan Kohler <stef@peekus.com>
2011-12-30 23:10:24 +0100

    Add first tests in python for reading from file system and playing around with gdata API

---
+++ core/reader.py
@@ -0,0 +1,8 @@
+import os
+
+path = '/Volumes/Multimedia/'
+listing = os.listdir(path)
+
+for infile in listing:
+    if not infile.startswith( '.' ):
+        print "Current file is: " + infile
---
+++ core/testgdataapi.py
@@ -0,0 +1,15 @@
+import gdata.youtube
+import gdata.youtube.service
+
+yt_service = gdata.youtube.service.YouTubeService()
+query = gdata.youtube.service.YouTubeVideoQuery()
+query.vq = 'avatar, movie, trailer, official'
+query.orderby = 'viewCount'
+query.racy = 'include'
+feed = yt_service.YouTubeQuery(query)
+
+for entry in feed.entry: 
+    if entry.media.duration.seconds > 60:
+        if "Avatar" in entry.media.title.text:
+            print entry.media.title.text
+            print entry.GetSwfUrl()
