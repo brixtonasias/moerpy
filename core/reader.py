68cb5a31da6d8e98fa7cbdea8eaf6517a8d6f5ce
Stefan Kohler <stef@peekus.com>
2012-01-06 00:54:45 +0100

    Adding reading path from command line parameter

--- core/reader.py
+++ core/reader.py
@@ -1,8 +1,15 @@
 import os
+from sys import argv
+
+script_name, folder_to_search = argv
+
+print folder_to_search
 
 path = '/Volumes/Multimedia/'
-listing = os.listdir(path)
+listing = os.listdir(folder_to_search)
 
 for infile in listing:
     if not infile.startswith('.'):
+        # TODO Call youtube API here to get info on trailers
+        # TODO then hand this over to parsing the/a template
         print "Current file is: " + infile
